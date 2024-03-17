62bdda020ea9a6a78ec7f8879457bc175b677ef0
d0ugherty <tdougherty16@gmail.com>
2024-03-17 14:32:41 -0400

    Get stops by agency

--- Controllers/GtfsController.cs
+++ Controllers/GtfsController.cs
@@ -2,8 +2,6 @@
 using Microsoft.AspNetCore.Mvc;
 using GtfsApi.Models;
 using Microsoft.EntityFrameworkCore;
-using System.Linq;
-using GtfsApi.Migrations;
 
 
 namespace GtfsApi.Controllers
@@ -23,6 +21,7 @@ namespace GtfsApi.Controllers
         public async Task<ActionResult<IEnumerable<Agency>>> GetAgencies()
         {
             var agencies = await _context.Agencies.ToListAsync();
+            
             return agencies;
         }
 
@@ -32,6 +31,7 @@ namespace GtfsApi.Controllers
             var routes = await _context.GtfsRoutes
                 .Where(rt => rt.AgencyName == agencyId.ToUpper())
                 .ToListAsync();
+            
             return routes;
         }
         // Need to finish this. I'll need to get more creative since this is timing out
@@ -39,26 +39,32 @@ namespace GtfsApi.Controllers
         [HttpGet("Stops/{agencyId}")]
         public async Task<ActionResult<IEnumerable<object>>> GetAgencyStops(string agencyId)
         {
-            
+            var routes = await (_context.GtfsRoutes
+                    .Where(rt => rt.AgencyName == agencyId.ToUpper()))
+                    .ToListAsync();
+
+            var routeIds = routes.Select(route => route.Id).ToList();
+
+            var trips = await (_context.Trips
+                    .Where(trip => routeIds.Contains(trip.RouteId))
+                    .GroupBy(trip => trip.RouteId)
+                    .Select(id => id.FirstOrDefault()))
+                    .ToListAsync();
+
+            var tripIds = trips.Select(trip => trip!.Id).ToList();
+
+            var stopTimes = await (_context.StopTimes
+                    .Where(stopTime => tripIds.Contains(stopTime.TripId))
+                    .GroupBy(stopTime => stopTime.StopId)
+                    .Select(stopId => stopId.FirstOrDefault()))
+                    .ToListAsync();
+
+            var stopIds = stopTimes.Select(stopTime => stopTime!.StopId).ToList();
+
             var stops = await (_context.Stops
-                .Join(_context.StopTimes,
-                stop => stop.Id,
-                stopTime => stopTime.StopId,
-                (stop, stopTime) => new { stop, stopTime })
-                .Join(_context.Trips,
-                    joined => joined.stopTime.TripId,
-                    trip => trip.Id,
-                    (joined, trip) => new {joined.stop, joined.stopTime, trip})
-                .Join(_context.GtfsRoutes,
-                    joined => joined.trip.RouteId,
-                    route => route.Id,
-                    (joined, route) => new {joined.trip, route})
-                .Join(_context.Agencies,
-                    joined => joined.route.AgencyId,
-                    agency => agency.Id,
-                    (joined, agency) => new {joined.route, agency})
-                .Where(joined => joined.agency.AgencyId == agencyId))
-                .ToListAsync();
+                .Where(stop => stopIds.Contains(stop.Id))
+                .Select(stop => stop)).ToListAsync();
+            
             return stops;
         }
     }
